68322a50d81af97242f1b18a031058c2f24a454f
ago106 <168357108+ago106@users.noreply.github.com>
2025-09-23 17:13:14 +0700

    Update SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService("HttpService")
 
 local SaveManager = {} do
-	SaveManager.Folder = "FluentSettings"
+	SaveManager.Folder = "FenixSettings"
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
